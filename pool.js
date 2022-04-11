5ed997988f2c42230806ced50f10872de8495a06
Asymmentric <74967082+Asymmentric@users.noreply.github.com>
2022-04-11 21:34:27 +0530

    Update pool.js

--- pool.js
+++ pool.js
@@ -1,11 +1,13 @@
 const mysql=require('mysql');
+const dotenv=require('dotenv')
 
+dotenv.config()
 let pool=mysql.createPool({
     connectionLimit:100,
-    host:'localhost',
-    user:'root',
-    password:'1234',
-    database:'testooo'
+    host:'process.env.DB_HOST',
+    user:'process.env.DB_USER',
+    password:'process.env.DB_PWD',
+    database:'process.env.DB_NAME'
 })
 
 module.exports=pool;
